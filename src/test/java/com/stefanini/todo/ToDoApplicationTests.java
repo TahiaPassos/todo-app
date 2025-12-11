0dee900cf8fd8ab8dc1725cf65b7a6a46057d26b
Tahiana <tahiana.passos@hotmail.com>
2025-12-11 16:18:52 -0300

    corrigir nome da migration

--- src/test/java/com/stefanini/todo/ToDoApplicationTests.java
+++ src/test/java/com/stefanini/todo/ToDoApplicationTests.java
@@ -1,15 +1,15 @@
-package com.stefanini.todo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class ToDoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.stefanini.todo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class ToDoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
